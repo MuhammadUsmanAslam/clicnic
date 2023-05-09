941c881a0de7d2be475ce755e3c46c4e627f70a9
ikoote1 <ikootepreim@gmail.com>
2023-05-09 15:39:22 +0300

    database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,4 @@
 CREATE TABLE patients (id INT GENERATED ALWAYS AS IDENTITY, name varchar(100), date_of_birth DATE, PRIMARY KEY(id));
 
 CREATE TABLE medical_histories (id INT GENERATED ALWAYS AS IDENTITY, admitted_at TIMESTAMP, patient_id INT, status varchar(250), PRIMARY KEY(id), CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id));
+CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY, type VARCHAR(250), name VARCHAR(250), PRIMARY KEY(id));
