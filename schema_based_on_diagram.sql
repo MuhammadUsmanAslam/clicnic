ef7e04f51fe8fff8ba5cd06ca54e0d1252f987c7
ikoote1 <ikootepreim@gmail.com>
2023-05-09 15:40:11 +0300

    database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,3 +2,6 @@ CREATE TABLE patients (id INT GENERATED ALWAYS AS IDENTITY, name varchar(100), d
 
 CREATE TABLE medical_histories (id INT GENERATED ALWAYS AS IDENTITY, admitted_at TIMESTAMP, patient_id INT, status varchar(250), PRIMARY KEY(id), CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id));
 CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY, type VARCHAR(250), name VARCHAR(250), PRIMARY KEY(id));
+
+/* Many-to-Many relationship between trearments and medical-histories  */
+CREATE TABLE trearment_histories (medical_histoy_id INT,treatment_id INT,PRIMARY KEY (medical_histoy_id, treatment_id), CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy_id) REFERENCES medical_histories(id), CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id));
