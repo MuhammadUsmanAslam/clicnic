fdad6d41896745f62fd5394715875e67a2bb42cc
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-05-09 19:58:33 +0500

    Fix typos and add indexes for treatment_histories table foriegn-keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,7 +4,7 @@ CREATE TABLE medical_histories (id INT GENERATED ALWAYS AS IDENTITY, admitted_at
 CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY, type VARCHAR(250), name VARCHAR(250), PRIMARY KEY(id));
 
 /* Many-to-Many relationship between trearments and medical-histories  */
-CREATE TABLE trearment_histories (medical_histoy_id INT,treatment_id INT,PRIMARY KEY (medical_histoy_id, treatment_id), CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy_id) REFERENCES medical_histories(id), CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id));
+CREATE TABLE treatment_histories (medical_history_id INT,treatment_id INT,PRIMARY KEY (medical_history_id, treatment_id), CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy_id) REFERENCES medical_histories(id), CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id));
 
 CREATE TABLE invoices (id INT GENERATED ALWAYS AS IDENTITY,total_amount NUMERIC(5,2),generated_at TIMESTAMP,payed_at TIMESTAMP,medical_histoy__id INT DEFAULT 0,PRIMARY KEY(id),UNIQUE(medical_histoy__id),CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy__id) REFERENCES medical_histories(id));
 
@@ -15,3 +15,6 @@ CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
 CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
 CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+
+CREATE INDEX treatment_histories_medical_history_id_asc ON treatment_histories(medical_history_id ASC);
+CREATE INDEX treatment_histories_medical_history_id_asc ON treatment_histories(treatment_id ASC);
