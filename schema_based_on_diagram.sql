267864fb918c65a1d1e2c59b9b5eca75c62069f7
ikoote1 <ikootepreim@gmail.com>
2023-05-09 15:41:25 +0300

    database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,8 @@ CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY, type VARCHAR(250),
 
 /* Many-to-Many relationship between trearments and medical-histories  */
 CREATE TABLE trearment_histories (medical_histoy_id INT,treatment_id INT,PRIMARY KEY (medical_histoy_id, treatment_id), CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy_id) REFERENCES medical_histories(id), CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id));
+
+CREATE TABLE invoices (id INT GENERATED ALWAYS AS IDENTITY,total_amount NUMERIC(5,2),generated_at TIMESTAMP,payed_at TIMESTAMP,medical_histoy__id INT DEFAULT 0,PRIMARY KEY(id),UNIQUE(medical_histoy__id),CONSTRAINT fk_medical_histoy FOREIGN KEY(medical_histoy__id) REFERENCES medical_histories(id));
+
+
+
